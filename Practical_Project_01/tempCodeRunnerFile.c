85c4276a129c19a1f585515cf72b287c869b8bf3
Gustavo Guntzel <gustavo.g.s01@hotmail.com>
2021-04-26 18:11:08 -0300

    Deleting extra files

--- Practical_Project_01/tempCodeRunnerFile.c
+++
@@ -1,9 +0,0 @@
-if (validation == 0)
-    // {
-    //     printf("Por favor digite um número válido\n");
-    //     getNumberOfSessions();
-    // }
-    // else
-    // {
-    //     return numberOfsessions;
-    // }
