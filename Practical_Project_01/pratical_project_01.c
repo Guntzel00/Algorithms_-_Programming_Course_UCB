184ae1f2df9bb1bd208d5ed53d4b78f3d4f99712
Gustavo Guntzel <gustavo.g.s01@hotmail.com>
2021-04-26 17:36:26 -0300

    Project concluded

--- Practical_Project_01/pratical_project_01.c
+++ Practical_Project_01/pratical_project_01.c
@@ -8,6 +8,7 @@ Curso: Algoritimos de programacao
 #include <locale.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 // ----- Main Function ------
 
@@ -20,18 +21,18 @@ int main()
 
     char movieNameInput[50];
 
-    int numberOfViewers = 0;
+    int numberOfViewers;
 
-    int childViewers;
-    int teenagerViewers;
-    int adultViewers;
-    int seniorViewers;
+    int childViewers = 0;
+    int teenagerViewers = 0;
+    int adultViewers = 0;
+    int seniorViewers = 0;
 
-    int underEighteen;
-    int overEighteen;
+    int eighteenAndMale = 0;
+    int eighteenAndFemale = 0;
 
-    int maleViewers;
-    int femaleViewers;
+    int maleViewers = 0;
+    int femaleViewers = 0;
 
     // ------ Inputs ------
 
@@ -48,15 +49,99 @@ int main()
         fgets(movieNameInput, 50, stdin);
         fflush(stdin);
 
-        printf("%s", movieNameInput);
-
-    } while (strlen(movieNameInput) == 0);
+    } while ((strlen(movieNameInput) - 1) == 0);
 
     do
     {
-        printf("Digite o número de pessoas que assistiram o filme:\n");
-        scanf("%d", numberOfViewers);
+        printf("Digite o número de pessoas que assistiram ao filme:\n");
+        scanf("%d", &numberOfViewers);
         fflush(stdin);
-        printf()
-    } while (numberOfViewers < 10);
+    } while (numberOfViewers < 5 || numberOfViewers > 100);
+
+    for (int i = 0; i < numberOfViewers; i++)
+    {
+        int viewerAge;
+        char viewerSex;
+
+        do
+        {
+
+            printf("Digite a idade do %dº espectador:\n", i + 1);
+            scanf("%d", &viewerAge);
+            fflush(stdin);
+        } while (!isdigit(viewerAge) && (viewerAge <= 2 || viewerAge > 100));
+
+        do
+        {
+            printf("Digite o sexo do %dº espectador, use a letra [M] para masculino e [F] para feminino:\n", i + 1);
+            scanf("%c", &viewerSex);
+            viewerSex = toupper(viewerSex);
+            fflush(stdin);
+
+        } while (viewerSex != 'M' && viewerSex != 'F');
+
+        switch (viewerSex)
+        {
+        case 'F':
+            femaleViewers++;
+            break;
+        case 'M':
+            maleViewers++;
+            break;
+
+        default:
+            printf("Sexo não informado.");
+            break;
+        }
+
+        if (viewerAge >= 65)
+        {
+            seniorViewers++;
+            if (viewerSex == 'F')
+            {
+                eighteenAndFemale++;
+            }
+            else
+            {
+                eighteenAndMale++;
+            }
+        }
+        else if (viewerAge >= 14 && viewerAge < 18)
+        {
+            teenagerViewers++;
+        }
+        else if (viewerAge >= 18 && viewerAge < 65)
+        {
+            adultViewers++;
+
+            if (viewerSex == 'F')
+            {
+                eighteenAndFemale++;
+            }
+            else
+            {
+                eighteenAndMale++;
+            }
+        }
+        else
+        {
+            childViewers++;
+        }
+    }
+
+    system("clear");
+
+    printf("Nome do filme: %s", movieNameInput);
+    printf("Número de espectadores: %d\n", numberOfViewers);
+    printf("Número de espectadores homens: %d\n", maleViewers);
+    printf("Número de espectadores mulheres: %d\n", femaleViewers);
+    printf("Número de espectadores crianças: %d\n", childViewers);
+    printf("Número de espectadores Adolescentes: %d\n", teenagerViewers);
+    printf("Número de espectadores Adultos: %d\n", adultViewers);
+    printf("Número de espectadores Idosos: %d\n", seniorViewers);
+
+    // system("clear");
+
+    printf("Número de espectadores homens e maiores de idade: %d\n", eighteenAndMale);
+    printf("Número de espectadores mulheres e maiores de idade: %d\n", eighteenAndFemale);
 }
